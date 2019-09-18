7afe6ec5e627c5760157e18f5e9a12f08ecfcca3
loresalm <37289959+loresalm@users.noreply.github.com>
2019-09-18 22:14:54 +0200

    first simple synth

---
+++ synth.js
@@ -0,0 +1,27 @@
+
+let loopBeat
+let bassSynth
+
+function preload(){
+
+	bassSynth = new Tone.MembraneSynth().toMaster()
+
+	loopBeat = new Tone.Loop(song, '4n')
+
+}
+
+function setup() {
+	Tone.Transport.bpm.value = 180
+	Tone.Transport.start()
+	loopBeat.start(0)
+}
+
+function song(time){
+	console.log(time)
+	bassSynth.triggerAttackRelease('c1', '8n', time)
+
+	if (time >= 12){
+		Tone.Transport.bpm.value = 70
+	}
+
+}
